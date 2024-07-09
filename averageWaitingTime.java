8360976582ccc3ac0b89cb729b2c3e2b7948d91f
AryanDodke <aryandodke2003@gmail.com>
2024-07-09 15:52:55 +0530

    Daily Problem

---
+++ averageWaitingTime.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        int n = customers.length;
+        int[] time = new int[n];
+        int[] wait = new int[n];
+        int t = 0;
+        for (int i = 0; i < n; i++) {
+            t = Math.max(t, customers[i][0]) + customers[i][1];
+            time[i] = t;
+            wait[i] = time[i] - customers[i][0];
+        }
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += wait[i];
+        }
+        return sum / n;
+    }
+}
