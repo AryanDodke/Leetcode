ce428b5039ed8b530bd77f1c9b18abf484fa2a25
AryanDodke <aryandodke2003@gmail.com>
2024-08-12 08:08:12 +0530

    Leetcode Daily

---
+++ KthLargest.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+
+public class KthLargest {
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        minHeap = new PriorityQueue<>();
+        
+        // Initialize the heap with the first k elements
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        minHeap.offer(val);
+        if (minHeap.size() > k) {
+            minHeap.poll(); // Remove the smallest element
+        }
+        return minHeap.peek(); // The root element is the kth largest
+    }
+}
